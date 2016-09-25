47e0e4898907463c2607450947d98009d1b87c8f
Stephan Linz <linz@li-pro.net>
2016-09-25 19:25:11 +0200

    buildenv: build script for Xenial vanilla/next
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ Armbian_Cubietruck_next_Ubuntu_xenial.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Stephan Linz, linz@li-pro.net
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of tool chain https://github.com/lipro-armbian/build
+#
+#--------------------------------------------------------------------------------------------------------------------------------
+export LANG=""
+exec ./compile.sh local \
+	BOARD=cubietruck \
+	BRANCH=next \
+	RELEASE=xenial \
+	KERNEL_ONLY=no \
+	BUILD_DESKTOP=no \
+	ROOTFS_TYPE=ext4 \
+	COMPRESS_OUTPUTIMAGE=yes \
+	SEVENZIP=yes \
+	PROGRESS_DISPLAY=plain \
+	PROGRESS_LOG_TO_FILE=yes
